15b091d5e75783556f56f4ff93dcace846dee920
geraldo <geeferreirasd034@gmail.com>
2021-06-29 09:08:38 -0300

    gerando token de acesso

--- src/controllers/sessionController.ts
+++ src/controllers/sessionController.ts
@@ -2,6 +2,8 @@ import { Request, Response } from 'express'
 import { Session } from '../interface/Session'
 import { find_email } from '../services/userService'
 import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+// import secret from '../.env'
 
 
 export async function session (request: Request, response: Response): Promise<Response> {
@@ -12,13 +14,18 @@ export async function session (request: Request, response: Response): Promise<Re
         return response.json({'message': 'email ou senha estão incorretos'})
     }
 
+    //vericando senha com hash
     const hash = bcrypt.compareSync(sessionUser.password, user['password']);
     
     if(!hash){
-        return response.json({'message': 'email ou  senha estão incorretos'})
+        return response.json({'message': 'email ou senha estão incorretos'})
     }
     
     //gera o token
+    const token = jwt.sign({
+        id: user['id']
+    }, 'senha-secreta', { expiresIn: "1h" })
+    console.log('---', token)
 
-    return response.json({'message': 'session'})
+    return response.status(200).json({'message': token })
 }
